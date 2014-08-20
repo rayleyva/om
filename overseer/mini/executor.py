90e7d4ac752ceefaee1a7eac3b53325ceeaaa9a1
Thiago Santos <thiagossantos@gmail.com>
2014-08-20 01:24:41 -0300

    executor: add paramiko and client initialization

--- overseer/mini/executor.py
+++ overseer/mini/executor.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import paramiko
+
 class RemoteExecutor(object):
     '''Responsible for connecting and executing tasks remotely
 
@@ -11,10 +13,26 @@ class RemoteExecutor(object):
         self.tasks = []
         self.host = host
         self.username = username
+        self.client = None
 
     def add_task(self, t):
         self.tasks.append(t)
 
     def run(self):
+        '''Connects to remote computer and execute the tasks
+        '''
+        self.client = self._create_client()
+        self.client.connect(self.host, username=self.username)
         for t in self.tasks:
             print t
+        self.client.close()
+        self.client = None
+
+    def _create_client(self):
+        client = paramiko.client.SSHClient()
+        client.load_system_host_keys()
+
+        #FIXME remove this and expose an option to use it
+        client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+
+        return client
