42c1fc3ee5347c0bda4151d181e10080e17a976d
André Dieb Martins <andre.dieb@gmail.com>
2014-08-25 22:29:46 -0400

    Move handler loading to config

--- om/config.py
+++ om/config.py
@@ -2,11 +2,18 @@
 
 import json
 
+from om.handler import StdoutHandler, MailHandler
+from om.utils.logger import get_logger
+
+log = get_logger("config")
+
+
 class Config(object):
 
     def __init__(self, path):
         self._config = {}
         self._load_config(path)
+        self._handlers = []
 
     def __getitem__(self, key):
         return self._config.__getitem__(key)
@@ -18,3 +25,15 @@ class Config(object):
         if path is not None:
             with open(path) as config_file:
                 self._config = json.load(config_file)
+
+    @property
+    def handlers(self):
+        if not self._handlers:
+            self._handlers = [StdoutHandler()]
+            for handler, config in self.get('handlers', {}).iteritems():
+                if handler == 'email':
+                    self.handlers.append(MailHandler(**config))
+
+            log.debug('loaded handlers %s' % self.handlers)
+
+        return self._handlers
